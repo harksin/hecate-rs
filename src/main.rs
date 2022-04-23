4337ada0aae0c431361a206517f3373551bc624c
harksin <r.dardie@gmail.com>
2022-04-23 19:09:23 +0200

    :moon: init hecate-rs

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// trait HecateIds {
+//     type Ids;
+//     fn new() -> Self::Ids;
+//     fn toString() -> String;
+// }
+
+type AggregateId = String;
+type AggregateVersion = u64;
+
+trait EventStore<Event> {
+    fn write_event(aggregateId: AggregateId) -> AggregateVersion;
+    fn read_events(aggregateId: AggregateId) -> Vec<Event>;
+}
+
+struct AggregateRoot<Root> {
+    agg: Option<Root>,
+}
+
+trait Context {
+    type Aggregate;
+    type Command;
+    type Event;
+    type AggError;
+    fn empty() -> AggregateRoot<Self::Aggregate> {
+        AggregateRoot { agg: None }
+    }
+    fn apply(value: Self::Aggregate) -> AggregateRoot<Self::Aggregate> {
+        AggregateRoot { agg: Some(value) }
+    }
+
+    fn execute_command(
+        aggregate: AggregateRoot<Self::Aggregate>,
+        command: Self::Command,
+    ) -> Result<Self::AggError, Vec<Self::Event>>;
+
+    fn apply_event(
+        aggregate: AggregateRoot<Self::Aggregate>,
+        event: Self::Event,
+    ) -> AggregateRoot<Self::Aggregate>;
+    fn apply_history(
+        aggregate: AggregateRoot<Self::Aggregate>,
+        event: Vec<Self::Event>,
+    ) -> AggregateRoot<Self::Aggregate>;
+}
